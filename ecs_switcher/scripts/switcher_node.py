29c5c7fbfe2690802ee3f3b8e13a7cef7a39edf5
Petr Neduchal <neduchal@gmail.com>
2020-03-20 12:03:43 +0100

    [B] node called both the old and ther new function for starting processes. SOLVED

--- ecs_switcher/scripts/switcher_node.py
+++ ecs_switcher/scripts/switcher_node.py
@@ -6,10 +6,12 @@ import switcher_api
 from std_msgs.msg import String
 import cjson
 
+
 class SwitcherNode:
 
     def __init__(self, setting_file):
-        self.switch_sub = rospy.Subscriber("/switcher/switch", String, self.callback_switch)
+        self.switch_sub = rospy.Subscriber(
+            "/switcher/switch", String, self.callback_switch)
         self.active_process = None
         self.switcher_list = []
         self.default_lf = None
@@ -19,24 +21,23 @@ class SwitcherNode:
         for process in self.switcher_list:
             if (id == process[0]):
                 self.stop_process()
-                self.active_process = switcher_api.find_and_launch(lfs[1], lfs[2])        
-                self.active_process = switcher_api.start_process(process[1], process[2])
-                break   
+                self.active_process = switcher_api.start_process(
+                    process[1], process[2])
+                break
         else:
             rospy.logwarn("Launch File %s Not Found", id)
 
     def stop_process(self):
         switcher_api.stop_launch(self.active_process)
 
-
     def load_switcher_settings(self, filename):
         settings = cjson.load(filename)
         self.switcher_list = settings["lfs"]
         self.default_lf = settings["default_lf"]
-                
 
     def callback_switch(self, data):
         self.run_process(data.data)
+
 
 if __name__ == "__main__":
     # TODO: Add ros params loading
