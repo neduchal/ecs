9e85db044b4443fa42a228503693cb9027222a5b
Petr Neduchal <neduchal@gmail.com>
2020-03-20 11:46:48 +0100

    [REFACTORING] cjson script reformatting

--- ecs_switcher/scripts/cjson.py
+++ ecs_switcher/scripts/cjson.py
@@ -2,26 +2,23 @@
 # -*- coding: utf-8 -*-
 import json
 
+
 def load(filename):
     """
-        Nacteni komentovaneho JSON souboru
+        Loading of a commented json file
 
-        :param filename: cesta k souboru
-        :type filename: string
+        @param filename: path to json file
     """
 
     fp = open(filename, 'r')
-
     lines = fp.readlines()
-    data = "";
+    data = ""
     for line in lines:
         linestrip = line.lstrip()
         if linestrip[0:1] == '#':
             continue
         else:
             data = data + line
-
     data = json.loads(data)
-
     fp.close()
     return data
