037275373c791494a95a3a09c25d7b750dd9c3ff
Petr Neduchal <neduchal@gmail.com>
2020-03-20 12:17:35 +0100

    [M] Added params loading for settings file and for switcher subscriber topic. Use new switcher api stop process function

--- ecs_switcher/scripts/switcher_node.py
+++ ecs_switcher/scripts/switcher_node.py
@@ -9,38 +9,47 @@ import cjson
 
 class SwitcherNode:
 
-    def __init__(self, setting_file):
+    def __init__(self):
+
+        self.node_name = rospy.get_name()
+        self.settings_file = rospy.get_param(
+            self.node_name + "/settings_file", "")
+        if self.settings_file == "":
+            quit(0)
+        self.switcher_sub_topic = rospy.get_param(
+            self.node_name + "/switcher_sub_topic", "/ecs/switcher/switch")
         self.switch_sub = rospy.Subscriber(
-            "/switcher/switch", String, self.callback_switch)
+            self.switcher_sub_topic, String, self.callback_switch)
         self.active_process = None
+        self.active_process_type = None
         self.switcher_list = []
         self.default_lf = None
-        self.load_switcher_settings(setting_file)
+        self.load_switcher_settings(self.settings_file)
 
     def run_process(self, id):
         for process in self.switcher_list:
             if (id == process[0]):
                 self.stop_process()
-                self.active_process = switcher_api.start_process(
+                self.active_process, self.active_process_type = switcher_api.start_process(
                     process[1], process[2])
                 break
         else:
-            rospy.logwarn("Launch File %s Not Found", id)
+            rospy.logwarn("Process %s Not Found", id)
 
     def stop_process(self):
-        switcher_api.stop_launch(self.active_process)
+        switcher_api.stop_process(
+            self.active_process, self.active_process_type)
 
     def load_switcher_settings(self, filename):
         settings = cjson.load(filename)
-        self.switcher_list = settings["lfs"]
-        self.default_lf = settings["default_lf"]
+        self.switcher_list = settings["processes"]
+        self.default_lf = settings["default_process"]
 
     def callback_switch(self, data):
         self.run_process(data.data)
 
 
 if __name__ == "__main__":
-    # TODO: Add ros params loading
     rospy.init_node("ecs_switcher_node")
-    switcher_node = SwitcherNode("")
+    switcher_node = SwitcherNode()
     rospy.spin()
