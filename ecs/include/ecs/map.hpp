512f9ee5366667eb2a32241cae9df7ddf3919ff9
Petr Neduchal <neduchal@gmail.com>
2020-03-26 12:16:03 +0100

    [M] Change the way of publishing GRID MAP from map_node

--- ecs/include/ecs/map.hpp
+++ ecs/include/ecs/map.hpp
@@ -29,52 +29,54 @@
 #include <ecs/GetLayers.h>
 #include <ecs/GetLayer.h>
 
-using namespace grid_map; 
+using namespace grid_map;
 
-namespace ecs 
+namespace ecs
 {
-    const std::string   BASE_LAYER              =                  "base";    
-    const std::string   DEFAULT_MAP_TOPIC       =                   "map";
-    const std::string   DEFAULT_ENV_VALUE_TOPIC =        "/ecs/env_value";  
-    const std::string   DEFAULT_SET_LAYER_TOPIC =        "/ecs/set_layer";
-    const std::string   DEFAULT_GRID_MAP_TOPIC  =         "/ecs/grid_map";
-    const std::string   DEFAULT_BASE_FRAME      =             "base_link";
-    const std::string   DEFAULT_MAP_FRAME       =                   "map";
+W
+const double GRID_MAP_PUBLISH_OFF = 0.0;
+const std::string BASE_LAYER = "base";
+const std::string DEFAULT_MAP_TOPIC = "map";
+const std::string DEFAULT_ENV_VALUE_TOPIC = "/ecs/env_value";
+const std::string DEFAULT_SET_LAYER_TOPIC = "/ecs/set_layer";
+const std::string DEFAULT_GRID_MAP_TOPIC = "/ecs/grid_map";
+const std::string DEFAULT_BASE_FRAME = "base_link";
+const std::string DEFAULT_MAP_FRAME = "map";
+W
+class Map
+{
+public:
+    Map(ros::NodeHandle &node_handle);
+    virtual ~Map();
+    void readParameters();
+    void envValueCallback(const ecs::EnvValue &msg);
+    void inputMapCallback(const nav_msgs::OccupancyGrid &msg);
+    void getLayers(ecs::GetLayers::Request &req, ecs::GetLayers::Response &res);
+    void getLayer(ecs::GetLayer::Request &req, ecs::GetLayer::Response &res);
+    void setLayerCallback(const grid_map_msgs::GridMap &msg);
+    void publishGridMap();
+    void getPose();
 
-    class Map  
-    {
-        public:
-            Map(ros::NodeHandle& node_handle);
-            virtual ~Map();
-            void readParameters();
-            void envValueCallback(const ecs::EnvValue& msg);
-            void inputMapCallback(const nav_msgs::OccupancyGrid& msg);
-            void getLayers(ecs::GetLayers::Request &req, ecs::GetLayers::Response &res);
-            void getLayer(ecs::GetLayer::Request &req, ecs::GetLayer::Response &res);
-            void setLayerCallback(const grid_map_msgs::GridMap& msg);
-            void publishGridMap();
-            void getPose();
-    
-        private:
-            ros::NodeHandle& nh_;
-            std::string env_value_topic_;
-            std::string input_map_topic_;
-            std::string set_layer_topic_;
-            std::string input_map_metadata_topic_;
-            std::string grid_map_topic_;
-            std::string base_frame_;
-            std::string map_frame_;
-            ros::Subscriber env_value_subscriber_;
-            ros::Subscriber input_map_subscriber_;
-            ros::Subscriber set_layer_subscriber_;
-            ros::Publisher grid_map_publisher_;
-            tf2_ros::TransformListener *transform_listener_;
-            tf2_ros::Buffer transformBuffer_;
-            geometry_msgs::TransformStamped pose_;
-            GridMap map_;
-            GridMapRosConverter converter_;
-            bool setup_done_;
-    };
-}
+private:
+    ros::NodeHandle &nh_;
+    std::string env_value_topic_;
+    std::string input_map_topic_;
+    std::string set_layer_topic_;
+    std::string input_map_metadata_topic_;
+    std::string grid_map_topic_;
+    std::string base_frame_;
+    std::string map_frame_;
+    ros::Subscriber env_value_subscriber_;
+    ros::Subscriber input_map_subscriber_;
+    ros::Subscriber set_layer_subscriber_;
+    ros::Publisher grid_map_publisher_;
+    tf2_ros::TransformListener *transform_listener_;
+    tf2_ros::Buffer transformBuffer_;
+    geometry_msgs::TransformStamped pose_;
+    GridMap map_;
+    GridMapRosConverter converter_;
+    bool setup_done_;
+};
+} // namespace ecs
 
 #endif
--- ecs/src/map.cpp
+++ ecs/src/map.cpp
@@ -11,153 +11,149 @@ using namespace grid_map;
 
 namespace ecs
 {
-    Map::Map(ros::NodeHandle &node_handle)
-        : nh_(node_handle)
-    {
-        readParameters();
-        env_value_subscriber_ = nh_.subscribe(env_value_topic_,
-                                                      100, 
-                                                      &Map::envValueCallback, 
-                                                      this
-                                                      );
-        input_map_subscriber_ = nh_.subscribe(input_map_topic_, 
-                                                      1, 
-                                                      &Map::inputMapCallback, 
-                                                      this
-                                                      );
-        set_layer_subscriber_ = nh_.subscribe(set_layer_topic_, 
-                                                      10, 
-                                                      &Map::setLayerCallback, 
-                                                      this
-                                                      );
-        grid_map_publisher_ = nh_.advertise<grid_map_msgs::GridMap>(grid_map_topic_, 1, true);
-        //map_ = GridMap({"base"});
-        //map_.setBasicLayers({"base"});
-        map_ = GridMap(ros::V_string({"BASE_LAYER"}));
-        map_.setBasicLayers(ros::V_string({"BASE_LAYER"}));
-        map_.setFrameId(map_frame_);
-        transform_listener_ = new tf2_ros::TransformListener(transformBuffer_);
-        converter_ = GridMapRosConverter();
-        setup_done_ = false;
-    }
 
-    Map::~Map()
-    {
-    }
+Map::Map(ros::NodeHandle &node_handle)
+    : nh_(node_handle)
+{
+    readParameters();
+    env_value_subscriber_ = nh_.subscribe(env_value_topic_,
+                                          100,
+                                          &Map::envValueCallback,
+                                          this);
+    input_map_subscriber_ = nh_.subscribe(input_map_topic_,
+                                          1,
+                                          &Map::inputMapCallback,
+                                          this);
+    set_layer_subscriber_ = nh_.subscribe(set_layer_topic_,
+                                          10,
+                                          &Map::setLayerCallback,
+                                          this);
+    grid_map_publisher_ = nh_.advertise<grid_map_msgs::GridMap>(grid_map_topic_, 1, true);
+    //map_ = GridMap({"base"});
+    //map_.setBasicLayers({"base"});
+    map_ = GridMap(ros::V_string({"BASE_LAYER"}));
+    map_.setBasicLayers(ros::V_string({"BASE_LAYER"}));
+    map_.setFrameId(map_frame_);
+    transform_listener_ = new tf2_ros::TransformListener(transformBuffer_);
+    converter_ = GridMapRosConverter();
+    setup_done_ = false;
+}
 
-    void Map::readParameters()
-    {
-        nh_.param<std::string>("input_map_topic", input_map_topic_,       DEFAULT_MAP_TOPIC);
-        nh_.param<std::string>("env_value_topic", env_value_topic_, DEFAULT_ENV_VALUE_TOPIC);
-        nh_.param<std::string>("set_layer_topic", set_layer_topic_, DEFAULT_SET_LAYER_TOPIC);
-        nh_.param<std::string>("base_frame",      base_frame_,           DEFAULT_BASE_FRAME);
-        nh_.param<std::string>("map_frame",       map_frame_,             DEFAULT_MAP_FRAME);
-        nh_.param<std::string>("grid_map_topic",  grid_map_topic_,   DEFAULT_GRID_MAP_TOPIC);
-    }
+Map::~Map()
+{
+}
 
-    void Map::envValueCallback(const ecs::EnvValue &msg)
+void Map::readParameters()
+{
+    nh_.param<std::string>("input_map_topic", input_map_topic_, DEFAULT_MAP_TOPIC);
+    nh_.param<std::string>("env_value_topic", env_value_topic_, DEFAULT_ENV_VALUE_TOPIC);
+    nh_.param<std::string>("set_layer_topic", set_layer_topic_, DEFAULT_SET_LAYER_TOPIC);
+    nh_.param<std::string>("base_frame", base_frame_, DEFAULT_BASE_FRAME);
+    nh_.param<std::string>("map_frame", map_frame_, DEFAULT_MAP_FRAME);
+    nh_.param<std::string>("grid_map_topic", grid_map_topic_, DEFAULT_GRID_MAP_TOPIC);
+}
+
+void Map::envValueCallback(const ecs::EnvValue &msg)
+{
+    if (!setup_done_)
+        return;
+    if (!map_.exists(msg.layer))
+    {
+        map_.add(msg.layer);
+    }
+    Position position(pose_.transform.translation.x, pose_.transform.translation.y);
+    Index indexPosition;
+    map_.getIndex(position, indexPosition);
+    if (isnan(map_.at(msg.layer, indexPosition)))
+    {
+        map_.at(msg.layer, indexPosition) = msg.value;
+    }
+    else
+    {
+        map_.at(msg.layer, indexPosition) += msg.value;
+        map_.at(msg.layer, indexPosition) /= 2.0;
+    }
+}
+
+void Map::getPose()
+{
+    try
+    {
+        pose_ = transformBuffer_.lookupTransform(map_frame_, base_frame_, ros::Time(0));
+    }
+    catch (tf2::TransformException &ex)
+    {
+        ROS_WARN("%s", ex.what());
+        ros::Duration(1.0).sleep();
+    }
+}
+
+void Map::inputMapCallback(const nav_msgs::OccupancyGrid &msg)
+{
+    if (!converter_.fromOccupancyGrid(msg, BASE_LAYER, map_))
+    {
+        ROS_ERROR("Could not transform occupancy grid into the GridMap");
+    }
+    else
     {
         if (!setup_done_)
-            return;
-        if (!map_.exists(msg.layer))
-        {
-            map_.add(msg.layer);
-        }
-        Position position(pose_.transform.translation.x, pose_.transform.translation.y);
-        Index indexPosition;
-        map_.getIndex(position, indexPosition);
-        if (isnan(map_.at(msg.layer, indexPosition)))
-        {
-            map_.at(msg.layer, indexPosition) = msg.value; 
-        } 
-        else 
-        {
-            map_.at(msg.layer, indexPosition) += msg.value;
-            map_.at(msg.layer, indexPosition) /= 2.0;
-        }
+            setup_done_ = true;
+        Map::getPose();
     }
+}
 
-    void Map::getPose()
+void Map::publishGridMap()
+{
+    ros::Time time = ros::Time::now();
+    map_.setTimestamp(time.toNSec());
+    grid_map_msgs::GridMap msg;
+    converter_.toMessage(map_, msg);
+    grid_map_publisher_.publish(msg);
+    ROS_INFO_THROTTLE(1.0, "Grid map (timestamp %f) published.", msg.info.header.stamp.toSec());
+}
+
+void Map::setLayerCallback(const grid_map_msgs::GridMap &msg)
+{
+    GridMap grid_map_layer = GridMap();
+    converter_.fromMessage(msg, grid_map_layer);
+    std::vector<std::string> layers = grid_map_layer.getLayers();
+    for (int i; i < layers.size(); i++)
     {
-        try
+        if (layers[i].compare(BASE_LAYER) > 0)
         {
-            pose_ = transformBuffer_.lookupTransform(map_frame_, base_frame_, ros::Time(0));
-        }
-        catch (tf2::TransformException &ex)
-        {
-            ROS_WARN("%s", ex.what());
-            ros::Duration(1.0).sleep();
+            map_.add(layers[i], grid_map_layer.get(layers[i]));
         }
     }
+}
 
-    void Map::inputMapCallback(const nav_msgs::OccupancyGrid &msg)
+void Map::getLayers(ecs::GetLayers::Request &req, ecs::GetLayers::Response &res)
+{
+    std::vector<std::string> layers = map_.getLayers();
+    for (std::size_t i = 0; i < layers.size(); ++i)
     {
-        if (!converter_.fromOccupancyGrid(msg, BASE_LAYER, map_))
-        {
-            ROS_ERROR("Could not transform occupancy grid into the GridMap");
-        }
-        else
-        {
-            /*ROS_INFO("MAP -- size (%f x %f m), %i x %i cells -- center (%f, %f) -- frame %s", map_.getLength().x(), map_.getLength().y(),
-                    map_.getSize()(0), map_.getSize()(1), map_.getPosition().x(), map_.getPosition().y(), map_.getFrameId().c_str());*/
-            if (!setup_done_)
-                setup_done_ = true;
-            Map::getPose();
-        }
+        res.layers.push_back(layers[i]);
     }
+}
 
-    void Map::publishGridMap()
+void Map::getLayer(ecs::GetLayer::Request &req, ecs::GetLayer::Response &res)
+{
+    GridMap map_msg = GridMap();
+    grid_map_msgs::GridMap msg;
+
+    if (map_.exists(req.layer))
     {
-        ros::Time time = ros::Time::now();
-        map_.setTimestamp(time.toNSec());
-        grid_map_msgs::GridMap msg;
-        converter_.toMessage(map_, msg);
-        grid_map_publisher_.publish(msg);
-        ROS_INFO_THROTTLE(1.0, "Grid map (timestamp %f) published.", msg.info.header.stamp.toSec());
+        std::vector<std::string> layers;
+        layers.push_back(BASE_LAYER);
+        layers.push_back(req.layer);
+        res.valid = true;
+        map_msg.addDataFrom(map_, true, true, false, layers);
+        converter_.toMessage(map_msg, msg);
+        res.map = msg;
     }
-
-    void Map::setLayerCallback(const grid_map_msgs::GridMap &msg)
+    else
     {
-        GridMap grid_map_layer = GridMap();
-        converter_.fromMessage(msg, grid_map_layer);
-        std::vector<std::string> layers = grid_map_layer.getLayers();
-        for (int i; i < layers.size(); i++)
-        {
-            if (layers[i].compare(BASE_LAYER) > 0)
-            {
-                map_.add(layers[i], grid_map_layer.get(layers[i]));
-            }
-        }
+        res.valid = false;
     }
-
-    void Map::getLayers(ecs::GetLayers::Request &req, ecs::GetLayers::Response &res)
-    {
-        std::vector<std::string> layers = map_.getLayers();
-        for (std::size_t i = 0; i < layers.size(); ++i)
-        {
-            res.layers.push_back(layers[i]);
-        }
-    }
-
-    void Map::getLayer(ecs::GetLayer::Request &req, ecs::GetLayer::Response &res)
-    {
-        GridMap map_msg = GridMap();
-        grid_map_msgs::GridMap msg;
-
-        if (map_.exists(req.layer))
-        {
-            std::vector<std::string> layers;
-            layers.push_back(BASE_LAYER);
-            layers.push_back(req.layer);
-            res.valid = true;
-            map_msg.addDataFrom(map_, true, true, false, layers);
-            converter_.toMessage(map_msg, msg);
-            res.map = msg;
-        }
-        else
-        {
-            res.valid = false;
-        }
-    }
+}
 
 } // namespace ecs
